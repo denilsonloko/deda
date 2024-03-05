3bd42c4f7f37599c5e94ad19c7a72d4206f2f014
Denilson <160132544+denilsonloko@users.noreply.github.com>
2024-03-05 10:25:16 -0300

    Add files via upload

---
+++ js/comandos.js
@@ -0,0 +1,12 @@
+
+
+$(".ban").on("mouseover", function () {
+    // console.log("mouse em cima"); 
+    var src = $(this).attr("src"); 
+   $("#imageZoom").attr("src" , src);
+
+});  // console.log(" caminho ")funçao para pquando o mouser passar em cimas da imagem confugurada , dar a mensagem mouse em cima //
+
+    //$("#principal").attr("src" , src); atribui para trocar as imagens //
+
+
